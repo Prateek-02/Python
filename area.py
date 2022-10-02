765a4f89756d102f2e836aeb907dee1f34a17d92
PRATEEK\prate <prateekrajgrd74@gmail.com>
2022-10-02 16:13:33 +0530

    sqrt.py

--- area.py
+++
@@ -1,8 +0,0 @@
-#print area of triangle using heron's formula
-import math
-a,b,c = 6,2,6
-s = (a+b+c)/2
-area = math.sqrt(s*(s-a)*(s-b)*(s-c))
-print('area of triangle:',a,b,c)
-print('area:',area,'square units')
-
---
+++ sqrt.py
@@ -0,0 +1,6 @@
+#find sqrt of any number
+import math
+num=int(input("enter the square root number:"))
+square = num**(1/2)
+print("square root of", num,"is",square)
+
