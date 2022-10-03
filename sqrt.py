df7dc2e5727ce2b1b132d8a990b9346be2d739cd
PRATEEK\prate <prateekrajgrd74@gmail.com>
2022-10-03 19:18:15 +0530

    sqrt.py

--- sqrt.py
+++ sqrt.py
@@ -2,5 +2,9 @@
 import math
 num=int(input("enter the square root number:"))
 square = num**(1/2)
-print("square root of", num,"is",square)
+print("square root of",num,"is",square)
 
+import math
+num=int(input("enter the square root number:"))
+square = num**(1/2)
+print("square root of",num,"is",square)
