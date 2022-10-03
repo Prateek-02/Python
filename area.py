7dd66b1000a483bf6f91f57c85271336e2bcef6b
PRATEEK\prate <prateekrajgrd74@gmail.com>
2022-10-03 19:32:30 +0530

    area.py

--- area.py
+++ area.py
@@ -3,4 +3,11 @@ a,b,c = 6,9,14
 s = (a+b+c)/2
 area = (s*(s-a)*(s-b)*(s-c))
 print('sides of triangle:' ,a,b,c)
-print('area:',area,'square unit')
+print('area:',area,'square units')
+
+import math
+a,b,c = 10,12,14
+s = (a+b+c)/2
+area = (s*(s-a)*(s-b)*(s-c))
+print('sides of triangle:' ,a,b,c)
+print('area:' ,area,'square units')
