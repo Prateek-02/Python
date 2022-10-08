f21a1df32ebf409acb11881bd2b652aebdbc0663
PRATEEK\prate <prateekrajgrd74@gmail.com>
2022-10-08 16:36:37 +0530

    week.py

---
+++ week.py
@@ -0,0 +1,17 @@
+number = int(input("enter the number 1 to 7 to get the day:"))
+if number == 1:
+    print("sunday")
+elif number == 2:
+    print("monday")
+elif number == 3:
+    print("tuesday")        
+elif number == 4:
+    print("wednesday")
+elif number == 5:
+    print("thursday")    
+elif number == 6:
+    print("friday")
+elif number == 7:
+    print("saturday")       
+else :
+    print("enter a valid number")         
