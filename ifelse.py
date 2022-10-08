2c1825e6edb9d767cc778a378a7f5dcb8b7dc935
PRATEEK\prate <prateekrajgrd74@gmail.com>
2022-10-08 13:34:25 +0530

    ifelse.py

--- ifelse.py
+++ ifelse.py
@@ -1,7 +1,7 @@
-x = int(input("enter the value of x:"))
-y = int(input("enter the value of y:"))
+length = int(input("enter the value of length:"))
+breadth = int(input("enter the value of breadth:"))
 
-if x < y:
-    print("YAYY")
+if length == breadth : 
+    print("this is a square")
 else:
-    print("NAYY")            
+    print("this is not a square")            
