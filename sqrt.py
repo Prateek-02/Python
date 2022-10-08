4909e76d32a78b7d8205e8363157a1d0b43d88ce
PRATEEK\prate <prateekrajgrd74@gmail.com>
2022-10-08 12:38:36 +0530

    sqrt.py

--- sqrt.py
+++ sqrt.py
@@ -1,10 +1,5 @@
 #find sqrt of any number
 import math
-num=int(input("enter the square root number:"))
+num = int(input("enter the square root number:"))
 square = num**(1/2)
-print("square root of",num,"is",square)
-
-import math
-num=int(input("enter the square root number:"))
-square = num**(1/2)
-print("square root of",num,"is",square)
+print(square)
