74e846c6da9c620a8208c76b3444b80f96cab9de
PRATEEK\prate <prateekrajgrd74@gmail.com>
2022-10-15 16:50:40 +0530

    problems_string.py

---
+++ problems_string.py
@@ -0,0 +1,11 @@
+#take a string of length 5[index 0-4]
+#print first character
+#print middle character
+#print last character
+
+a = "Hello"
+print(a[0])
+b = (len(a)//2)
+print(a[b])
+c = "Hello"
+print(c[4])
