5909bbd5f53c48c359dd80ba75926d6ca54d121e
PRATEEK\prate <prateekrajgrd74@gmail.com>
2022-10-15 16:27:03 +0530

    stringslicing

--- stringslicing.py
+++ stringslicing.py
@@ -1,4 +1,8 @@
 a = "hello world"
 print(a.upper())
-b = "TONY STARK"
+b = "HELLO PRATEEK"
 print(b.lower())
+c = "GoOd MoRnInG"
+print(c.swapcase())
+d = "Today is saturday\n""Class is going onn"
+print(d)
