a6998c349cf33ff778004c50c133da9fcb751e81
PRATEEK\prate <prateekrajgrd74@gmail.com>
2022-10-16 16:32:20 +0530

    count.py

---
+++ count.py
@@ -0,0 +1,9 @@
+sentence = "beti bought some butter but the butter was bitter, so beti bought some more butter to make the bitter butter better, but when beti mixed the better butter with bitter butter the better butter becomes bitter butter"
+print(sentence.count("butter"))
+print(sentence.count("bitter"))
+print(sentence.count("beti"))
+print(sentence.count("better"))
+print(sentence.count("the"))
+print(sentence.count("bought"))
+print(sentence.count("some"))
+print(sentence.count("but"))
