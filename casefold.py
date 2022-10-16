60eaa4ca585794f2ac58d3e3f560f7d08941db38
PRATEEK\prate <prateekrajgrd74@gmail.com>
2022-10-16 16:17:35 +0530

    casefold.py

---
+++ casefold.py
@@ -0,0 +1,7 @@
+a = "THESKYISBLUE"
+print("lowercase string:" , a.casefold())
+
+b = "toDAY iS sUNdaY"
+print("original string:" , b)
+#print the string after casefold transformation
+print("string after using casefold():" , b.casefold())
