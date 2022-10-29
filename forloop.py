adedad3a6179f5e2b042a80f79b74446f8260652
Prateek-02 <Prateekrajgrd74@gmail.com>
2022-10-29 16:10:37 +0530

    forloop.py

--- forloop.py
+++ forloop.py
@@ -9,5 +9,19 @@
 #for i in str:
 #    print(i)
 
-for i in range(0,66,6):
-    print(i)
+#for i in range(0,66,6):
+#    print(i)
+
+#colors = ["red","blue","yellow"]
+#cars = ["Tata","BMW","Ford"]
+
+#for i in cars:
+#    for j in colors:
+#        print(j,i)
+
+name = ["ram","shyam","krishn"]
+color = ["white","brown","blue"]
+for i in name:
+    for j in color:
+        print(j,i)
+
