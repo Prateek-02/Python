d3c906ccf8beb61e21cb90a2168d1317ebbbf50c
Prateek-02 <Prateekrajgrd74@gmail.com>
2022-10-29 15:52:32 +0530

    forloop.py

--- forloop.py
+++ forloop.py
@@ -1,8 +1,10 @@
-#"BMW","Kia","Nano","Ford"]
+cars =["Tata","BMW","Kia","Nano","Ford"]
  
-#for i in cars:
-#    print(i)
+for i in cars:
+   if i == "Kia":
+       break;
+   print(i)
 
-str = "HELLO WORLD"
-for i in str:
-    print(i)
+#str = "HELLO WORLD"
+#for i in str:
+#    print(i)
