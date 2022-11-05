95a930a2478ed31a1216cf07a315d5e0930d88f3
Prateek-02 <Prateekrajgrd74@gmail.com>
2022-11-05 13:05:53 +0530

    function.py

---
+++ functionintro.py
@@ -0,0 +1,39 @@
+#def hello():
+#    print("hello world")
+#    print("good noon")
+#hello()    
+
+#def hello_world():
+#    print("hello world")
+
+#def hello_mars():
+#    print("hello mars")
+    
+#hello_world()        
+#hello_mars()
+
+#def name(x):
+#    print(x)
+#name(4)  
+#name(10)  
+
+#def add(num1,num2):
+#    sum = num1 + num2
+#    print(sum)
+#add(5,10)    
+#add(2,8)
+#add(9,10)
+
+"""def  sub(num1,num2):
+    diff = num1 - num2
+    print(diff)
+sub(10,8)
+sub(17,9)""" 
+
+def multi(num1,num2):
+    product = num1 * num2
+    return product
+
+result1 = multi(2,4)
+result2 = multi(9,10)
+print(result1,result2)
