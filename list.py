8ea75c6de0b2e71827f3726068193fbb11ade86d
Prateek-02 <Prateekrajgrd74@gmail.com>
2022-11-12 13:56:12 +0530

    list.py

---
+++ list.py
@@ -0,0 +1,15 @@
+colors = ["red","blue","red","black",22]
+cars = ["TATA","NANO","ALTO"]
+players = ("Dhoni","Messi","Dravid")
+#colors[2] = "Green"
+#colors[0:2] = "yellow","pink"
+#colors.insert(2,"indigo")
+#colors.append("BMW")
+#colors.extend(players)
+#colors.remove("red")
+#pop removes from the specific index
+#colors.pop(1)
+del colors[0]
+print(colors)
+
+
