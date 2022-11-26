c4acb16e8ad682144254165880e5e1fd6a8128d6
Prateek-02 <Prateekrajgrd74@gmail.com>
2022-11-26 16:49:57 +0530

    set.py

--- set.py
+++ set.py
@@ -4,8 +4,20 @@
 # we cannot duplicate items  
 
 
-# myset = {"car","bike","boat","car"}
-# print(myset)
+# myset = {"car","bike","boat"}
+# print(myset)       
 
-myset = {"car","bike","boat","car"}
-print(len(myset))
+# myset = {"car","bike","boat"}
+# print(len(myset))    #output = 3
+
+# myset = {"car","bike","boat"}
+# if "bike" in myset:
+#     print("true")    #output = true
+# else:
+#     print("false")  
+
+myset = {"car","bike","boat"} 
+myset.add(1)
+print(myset)
+
+
