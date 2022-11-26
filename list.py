22c47197ccd09b3e87f6438a74762f4ff30b2eb5
Prateek-02 <Prateekrajgrd74@gmail.com>
2022-11-26 12:42:04 +0530

    list.py

--- list.py
+++ list.py
@@ -1,15 +1,24 @@
 colors = ["red","blue","red","black",22]
 cars = ["TATA","NANO","ALTO"]
-players = ("Dhoni","Messi","Dravid")
+players = ("Dhoni","Messi","CR7")
+number = {1,2,3}
 #colors[2] = "Green"
 #colors[0:2] = "yellow","pink"
 #colors.insert(2,"indigo")
+#append adds element in the end
 #colors.append("BMW")
-#colors.extend(players)
+colors.extend(players)
+#remove,removes particular data type
 #colors.remove("red")
 #pop removes from the specific index
 #colors.pop(1)
-del colors[0]
+#del colors[0]
+#colors.clear()
+#colors.extend(number)
+#colors.sort()
+#it will sort the list in asscending order
+#colors.copy
+#it copies the list
 print(colors)
 
 
