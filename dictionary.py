adaf3be09b8be753c1eb4b900d90f694d5d55d28
Prateek-02 <Prateekrajgrd74@gmail.com>
2022-12-03 13:23:06 +0530

    doctionary.py

---
+++ dictionary.py
@@ -0,0 +1,20 @@
+#dictionary are orderd
+#chnagable
+#does not allow duplicate value
+mydictionary={
+    "name": "Prateek",
+    "age": "19",
+    "percentage": 96.3
+}
+# age = mydictionary.get("age")
+# keys = mydictionary.keys()
+#items = mydictionary.items()                    #it will print all the items present in the dictionary
+# print(age)
+# print(keys)
+#print(items)
+mydictionary["roll number"] = 11                  #it will add roll number to mydictionary
+# mydictionary.pop("age")                   
+mydictionary.popitem()                            #it removes last key and value
+print(mydictionary)           
+
+
