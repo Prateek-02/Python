258bfe1c59fca393f595ea2e0ca9227762290187
Prateek-02 <Prateekrajgrd74@gmail.com>
2022-12-03 15:40:04 +0530

    lambda.py

---
+++ lambda.py
@@ -0,0 +1,15 @@
+# def iseven(i):
+#     return i%2==0
+        
+# list1 = [3,4,10,18,66,13,22,45]
+
+# evenNum = list(filter(iseven,list1))
+# print(evenNum)
+
+list1 = [1,3,5,3,88,9,55,67,23]
+greater5 = list(filter(lambda i: i>5,list1))
+print(greater5)
+
+
+
+
